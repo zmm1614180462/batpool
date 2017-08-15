accf2303619b77ed8bca96800c1561118b849416
zmm <1614180462@qq.com>
2017-08-15 17:23:19 +0800

    新建环境

--- config/index.js
+++ config/index.js
@@ -32,7 +32,14 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      'api':{
+          target:'http://chainpool.dev.ailadui.net/user/Index/api',
+          changeOrigin: true,
+          pathRewrite: { '^/api': '/api' }
+
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
