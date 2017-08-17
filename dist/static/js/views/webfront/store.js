2f11bc1b968a94696d97f2c9b1ec0473acf09683
zmm <1614180462@qq.com>
2017-08-17 18:02:39 +0800

    新建环境

---
+++ dist/static/js/views/webfront/store.js
@@ -0,0 +1,1 @@
+webpackJsonp([14],{41:function(t,a,e){"use strict";Object.defineProperty(a,"__esModule",{value:!0});var o=e(1),n=e(3);o.a.use(n.b);var r={formdata:{}},s={change:function(t,a){var e=a.type,o=a.val;console.log(e,o),t.formdata[e]=o}},u={},c={formdata:function(t){return t.formdata}};a.default=new n.b.Store({state:r,mutations:s,actions:u,getters:c})}},[41]);
